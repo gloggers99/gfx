2cf31f2dcd47aa916e2c4de875cd1779d78a9cba
Lucas Marta <lucas.marta0799@gmail.com>
2025-07-19 11:47:46 -0700

    basic pipeline, hello world gl code, rename gfx class to core, basic gfx_test code.

--- gfx_test/main.cpp
+++ gfx_test/main.cpp
@@ -3,10 +3,12 @@
 /// Most of the time it will just be random code I throw together.
 ///
 
-#include <gfx/gfx.hpp>
+#include <gfx/core.hpp>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    auto core = gfx::core();
+    std::cout << "gfx_test\n";
+    gfx::core core { };
 
     return 0;
 }
